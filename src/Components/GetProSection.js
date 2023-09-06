d120d3db984d25a24041e51f1c1c1630a6e38471
jeffreyguezou <jeffreyrajcj@gmail.com>
2023-09-06 20:00:11 +0530

    get pro section in main page

---
+++ src/Components/GetProSection.js
@@ -0,0 +1,28 @@
+import prosection from "../Assets/ProSection.png";
+import { motion } from "framer-motion";
+const GetProSection = () => {
+  return (
+    <div className="proSection">
+      <div className="proText">
+        <div className="featureTitle">
+          <span>Get even more with pro</span>
+        </div>
+        <div className="featureText">
+          <span>
+            Get even more organized with receipt scanning, charts and graphs,
+            currency conversion, and more!
+          </span>
+        </div>
+        <motion.button
+          whileHover={{ scale: 0.9 }}
+          transition={{ duration: 0.1 }}
+          className="signUpBtn"
+        >
+          Sign Up
+        </motion.button>
+      </div>
+      <img src={prosection} alt="getpro"></img>
+    </div>
+  );
+};
+export default GetProSection;
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,12 +1,13 @@
-import Header from "./Header"
-import MainSection from "./MainSection"
+import GetProSection from "./GetProSection";
+import Header from "./Header";
+import MainSection from "./MainSection";
 const LandingPage = () => {
-    return (
-        <div>
-            <Header />
-            <MainSection />
-        </div>
-    )
-
-}
-export default LandingPage
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <GetProSection />
+    </div>
+  );
+};
+export default LandingPage;
